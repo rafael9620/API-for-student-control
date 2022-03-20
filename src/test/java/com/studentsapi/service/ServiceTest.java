b38625699d99d1cd78a26ee388f70e6efa1b9278
Francisco R Espinosa <franciscoreg9620@gmail.com>
2022-03-20 11:18:44 -0400

    refacotorizado el test

--- src/test/java/com/studentsapi/service/ServiceTest.java
+++ src/test/java/com/studentsapi/service/ServiceTest.java
@@ -1,87 +1,60 @@
 package com.studentsapi.service;
 
 import com.studentsapi.persitence.dto.StudentGetDto;
-import com.studentsapi.persitence.dto.StudentSaveDto;
 import com.studentsapi.persitence.entity.Student;
 import com.studentsapi.persitence.mapper.StudentMapper;
+import com.studentsapi.persitence.mapper.StudentMapperSaveEntity;
 import com.studentsapi.persitence.repository.StudentRepository;
-import java.time.LocalDateTime;
-import java.util.Arrays;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.Assertions;
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
 public class ServiceTest {
 
     @Mock
-    private StudentRepository repository;
+    private StudentRepository repositoryMock;
     @Mock
-    private StudentMapper mapper;
-
+    private StudentMapper mapperMock;
+    @Mock
+    private StudentMapperSaveEntity mapperSaveMock;
     @InjectMocks
-    private StudentService service;
-
-    private Student student;
-
-    private StudentGetDto studentGetDto;
-    
-    private StudentSaveDto saveStudent;
+    private StudentService studentService;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-
-        student = new Student();
-        student.setId(20L);
-        student.setName("Manuel");
-        student.setLastName("Alto");
-        student.setEmail("mauel@gmail.com");
-        student.setLastModifiedBy("admin");
-        student.setCreatedBy("admin");
-        student.setCreateDate(LocalDateTime.now());
-        student.setLastModifiedDate(LocalDateTime.now());
-        student.setVersion(1);         
-        
-        studentGetDto = new StudentGetDto();
-        studentGetDto.setId(1L);
-        studentGetDto.setName("Manuel");
-        studentGetDto.setLastName("Alto");
-        studentGetDto.setEmail("mauel@gmail.com");
-        studentGetDto.setLastModifiedBy("admin");
-        studentGetDto.setCreatedBy("admin");
-        studentGetDto.setCreateDate(String.valueOf(LocalDateTime.now()));
-        studentGetDto.setLastModifiedDate(String.valueOf(LocalDateTime.now()));
-        studentGetDto.setVersion(1);       
-        
-        
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
     }
 
-    @Test
-    void finAllStudent() {
-        when(repository.findAll()).thenReturn(Arrays.asList(student));
-        assertNotNull(repository.findAll());
+    @Nested
+    public class finAllStudent {
+        @Test
+        public void when_xx_then_success() throws Exception {
 
-    }
-    
-    @Test
-    void finAllStudentService() {
-        when(service.finAllStudent()).thenReturn(Arrays.asList(studentGetDto));
-        assertNotNull(service.finAllStudent());
-    }   
+            List<Student> findAllReturnValue = new ArrayList();
+            when(repositoryMock.findAll()).thenReturn(findAllReturnValue);
 
-    @Test
-    void findById() {
-        Optional<StudentGetDto> result = service.findById(1L);
-        assertEquals("1L", result.get().getId());
+            List<StudentGetDto> toDtoListReturnValue = new ArrayList();
+            when(mapperMock.toDtoList(new ArrayList())).thenReturn(toDtoListReturnValue);
+
+            //2.Act
+            List<StudentGetDto> finAllStudentRetReturnValue = studentService.finAllStudent();
+
+            //3.Assert
+            List<StudentGetDto> sutExpected = new ArrayList();
+            assertEquals(sutExpected, finAllStudentRetReturnValue);
+        }
     }
+
 }
